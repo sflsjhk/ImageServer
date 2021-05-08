3ad8f2a4db5dd3d8bae0c423f773c5c99a9adf95
Hekang Jia <48497574+sflsjhk@users.noreply.github.com>
2021-05-08 04:09:41 -0400

    flask restful api complete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flaskext.mysql import MySQL
-from flask import Flask
+from flask import Flask, render_template, jsonify,request
 
 app = Flask(__name__)
 
@@ -11,9 +11,20 @@ app.config['MYSQL_DB'] = "ion_image"
 
 mysql = MySQL(app)
 
-@app.route('/')
+# Get Patient's image from first_name, last_name
+@app.route('/', methods = ['GET'])
 def index():
-    return "Ion Image Server"
+    # Get Patient's first/last name from Restful API Json Request
+    patient_json = request.get_json()
+    first_name = patient_json['first_name']
+    last_name = patient_json['last_name']
+    return jsonify({'patient_name':patient_json})
+
+# Get Patient's images by patient_id
+@app.route('/patient/<int:patient_id>', methods = ['GET'])
+def getImageById(patient_id):
+    return jsonify({'result':patient_id})
+
 
 if __name__ == '__main__':
     app.run(debug = True)
